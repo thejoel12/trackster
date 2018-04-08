2283c6d152efc963e63b5058bd676ed71a9a328d
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-04-08 11:50:35 -0400

    step 12

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 var Trackster = {};
 
-
+$var API_KEY = 5fa973d5490fd9f31ab5b3abc7b16159;
 
 $('#searchicon').click(function(){
-  console.log($('#textinput').val());
+  Trackster.searchTracksByTitle($('#textinput').val());
 });
 
 /*
@@ -19,6 +19,17 @@ Trackster.renderTracks = function(tracks) {
   Given a search term as a string, query the LastFM API.
   Render the tracks given in the API query response.
 */
+
+$(document).ready(function () {
+    $.ajax({
+      url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key=5fa973d5490fd9f31ab5b3abc7b16159&format=json',
+      datatype: 'jsonp',
+      success: function(data) {
+        $('.weather').html('<p class="data">' + data.current_observation.weather + '</p>');
+      }
+    });
+  });
+});
 Trackster.searchTracksByTitle = function(title) {
 
 };

a809e640b26661b86ce0ed5cf888ab60b9a9fc76
Joel Borchardt <joelborchardt@Joels-MBP.cfl.rr.com>
2018-04-09 12:42:50 -0400

    array part almost done

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,17 +1,32 @@
-$(document).ready(function() {
+
 var Trackster = {};
 
-$var API_KEY = 5fa973d5490fd9f31ab5b3abc7b16159;
 
+$(document).ready(function () {
 $('#searchicon').click(function(){
   Trackster.searchTracksByTitle($('#textinput').val());
 });
+});
+
+const API_KEY = '5fa973d5490fd9f31ab5b3abc7b16159';
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  var htmlTrackRow =
+  '<div class="container trackfill" id="tracks">' +
+    '<div class="border">' +
+    '<a href="' + track.url + '" class="col-sm-1"><i class="far fa-play-circle"></i></a> ' +
+    '<span class="col-sm-1 col-sm-offset-1">1</span>' +
+    '<span class="col-sm-3">' + track.name + '</span>' +
+    '<span class="col-sm-2">' + track.artist + '</span>'+
+    '<span class="col-sm-2">' + track.album + '</span>' +
+    '<span class="col-sm-1">' + track.listeners + '</span>' +
+    '<span class="col-sm-1">' + track.length + '</span>' +
+  '</div>' +
+  '</div>';
 
 };
 
@@ -20,19 +35,13 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 
-$(document).ready(function () {
-    $.ajax({
-      url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key=5fa973d5490fd9f31ab5b3abc7b16159&format=json',
-      datatype: 'jsonp',
-      success: function(data) {
-        $('.weather').html('<p class="data">' + data.current_observation.weather + '</p>');
-      }
-    });
-  });
-});
+
 Trackster.searchTracksByTitle = function(title) {
-
-};
-
-
-});
+      $.ajax({
+        url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+        datatype: 'jsonp',
+        success: function() {
+          console.log('success');
+        }
+      });
+    };

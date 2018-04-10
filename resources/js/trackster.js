9be34d19389b3101d37dcd7912a9361c1378afa9
Joel Borchardt <joelborchardt@Joels-MBP.cfl.rr.com>
2018-04-10 12:07:57 -0400

    done

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -18,19 +18,18 @@ Trackster.renderTracks = function(tracks) {
 
   $trackList.empty();
 
-  for (var trackIndex = 0; trackIndex < tracks.legth; trackIndex++) {
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track=tracks[trackIndex];
   var mediumAlbumArt = track.image[1]["#text"];
   var htmlTrackRow =
-  '<div class="container trackfill" id="tracks">' +
-    '<div class="border">' +
-    '<a href="' + track.url + '" class="col-sm-1"><i class="far fa-play-circle"></i></a> ' +
-    '<span class="col-sm-1 col-sm-offset-1">' + 1 +  '</span>' +
+    '<div class="row trackslist">' +
+
+    '<a href="' + track.url + '" class="col-sm-1 col-sm-offset-2"><i class="far fa-play-circle"></i></a> ' +
     '<span class="col-sm-3">' + track.name + '</span>' +
     '<span class="col-sm-2">' + track.artist + '</span>'+
-    '<span class="col-sm-2">' + mediumAlbumArt + '</span>' +
+    '<span class="col-sm-2"><img src="' + mediumAlbumArt + '"/></span>' +
     '<span class="col-sm-1">' + track.listeners + '</span>' +
     '<span class="col-sm-1">' + track.length + '</span>' +
-  '</div>' +
   '</div>';
   $trackList.append(htmlTrackRow);
 };
@@ -46,7 +45,6 @@ Trackster.searchTracksByTitle = function(title) {
       $.ajax({
         url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
         success: function(response) {
-          console.log('sucess');
           Trackster.renderTracks(response.results.trackmatches.track);
         }
       });
